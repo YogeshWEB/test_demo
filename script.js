831c9facb3ca86c2413f784c0ea9ef43373c8b58
YogeshWEB <neemaker246@gmail.com>
2023-04-02 00:41:55 +0530

    function in script

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ console.log("Hello World")
 const root=document.getElementById("root")
 const para= document.createElement("p");
 const text1= document.createTextNode("Hello This is JS")
+const btn = document.createElement("button")
+const btntext = document.createTextNode("small")
+
+
 para.appendChild(text1)
 root.appendChild(para)
+btn.appendChild(btntext)
+root.appendChild(btn)
 para.classList.add("pclass")
+function small(){
+    
+    para.classList.remove("pclass")
+}
+btn.setAttribute("onclick","small()")
